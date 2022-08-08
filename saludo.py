e0103bdca05f9abfbd6e8d5a20e54c33b311121e
Luis Juarez <juarez2112@juarez2112.com>
2022-08-08 15:25:15 -0500

    Promedio de calificaciones

--- saludo.py
+++ saludo.py
@@ -1,3 +1,18 @@
-def saludo(name):
-    print(f"Hola {name} BIenvenido")
-saludo("Luis Juarez")
+def alumno(name):
+    print(f"Bienvenido {name} a la Escuela America Clinton")
+name=input("Digita tu Nombre: ")
+
+def calif():
+    i=5
+    num=0
+    prom_fin=0
+    while i>=1:
+        prom=int(input(f"Digita tus calificaciones {i}: "))
+        num=num+prom
+        prom_fin=num/5
+        i-=1
+    print(f"SU claificacion promedio es de: {prom_fin}")
+
+if __name__=='__main__':
+    alumno(name)
+    calif()
